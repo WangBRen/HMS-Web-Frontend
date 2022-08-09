4c8156e182cd2e340fcdda91913c9b3c25f88613
cocode <seeucoco@163.com>
2022-08-09 16:46:14 +0800

    fix: layout render

--- src/components/GlobalFooter/index.vue
+++ src/components/GlobalFooter/index.vue
@@ -6,7 +6,7 @@
       <!-- <a href="https://www.github.com/sendya/" target="_blank">@Sendya</a> -->
     </template>
     <template v-slot:copyright>
-      <a href="https://hms.yootane.com" target="_blank">Yootane</a>
+      <a href="https://hms.yootane.com" target="_blank">2022 © YOOTANE</a>
     </template>
   </global-footer>
 </template>
