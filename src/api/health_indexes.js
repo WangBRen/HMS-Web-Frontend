6876f4162c217f1c583a8450e0b461376095e176
cocode <seeucoco@163.com>
2022-08-11 10:45:26 +0800

    support index edit

--- src/api/health_indexes.js
+++ src/api/health_indexes.js
@@ -4,14 +4,10 @@ const Api = {
   ListAllIndexes: '/health-indexes',
   CreateIndexItem: (projectName) => `/health-indexes/${projectName}/items`,
   UpdateIndexItem: (projectName, id) => `/health-indexes/${projectName}/items/${id}`,
-  CreateProject: (projectName) => `/health-indexes/${projectName}`
-}
-
-export function createProject (projectName) {
-  return request({
-    url: Api.CreateProject(projectName),
-    method: 'post'
-  })
+  DeleteIndexItem: (projectName, id) => `/health-indexes/${projectName}/items/${id}`,
+  CreateProject: (projectName) => `/health-indexes/${projectName}`,
+  UpdateProject: (projectName) => `/health-indexes/${projectName}`,
+  DeleteProject: (projectName) => `/health-indexes/${projectName}`
 }
 
 export function listAllIndexes () {
@@ -20,6 +16,34 @@ export function listAllIndexes () {
     method: 'get'
   })
 }
+
+// projects
+
+export function createProject (projectName) {
+  return request({
+    url: Api.CreateProject(projectName),
+    method: 'post'
+  })
+}
+
+export function deleteProject (projectName) {
+  return request({
+    url: Api.DeleteProject(projectName),
+    method: 'delete'
+  })
+}
+
+export function updateProjectName (projectName, newProjectName) {
+  return request({
+    url: Api.UpdateProject(projectName),
+    method: 'put',
+    data: {
+      projectName: newProjectName
+    }
+  })
+}
+
+// items
 
 export function createIndexItem (projectName, payload) {
   return request({
@@ -34,5 +58,12 @@ export function updateIndexItem (projectName, itemId, payload) {
     url: Api.UpdateIndexItem(projectName, itemId),
     method: 'put',
     data: payload
+  })
+}
+
+export function deleteIndexItem (projectName, itemId) {
+  return request({
+    url: Api.DeleteIndexItem(projectName, itemId),
+    method: 'delete'
   })
 }
