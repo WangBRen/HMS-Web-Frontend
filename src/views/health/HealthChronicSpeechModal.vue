83ea83fcdaca5f47ee876b90545e5be28c92be82
liangyong <1044439745@qq.com>
2022-10-25 16:48:08 +0800

    feat: 新增话术功能，优化编辑慢病、分级、新建慢病功能

---
+++ src/views/health/HealthChronicSpeechModal.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <a-modal
+      :visible="speechVisible"
+      v-if="speechVisible"
+      :title="'编辑话术【'+speechInfo.name+'】'"
+      @ok="speechHandleOk"
+      @cancel="speechHandleCancel"
+      :width="700"
+    >
+      <div v-for="item in speechData" :key="item.id">
+        等级{{ item.level }}
+        话术模板:
+        <a-input v-model="item.template"></a-input>
+      </div>
+    </a-modal>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    speechVisible: {
+        type: Boolean,
+        default: false
+    },
+    speechData: {
+        type: Array,
+        default: function () {
+            return []
+        }
+    },
+    speechInfo: {
+        type: Object,
+        default: function () {
+            return {}
+        }
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+    speechHandleOk () {
+        // console.log('传进来的', this.speechData)
+        // console.log('传进来info', this.speechInfo)
+    },
+    speechHandleCancel () {
+        this.$emit('closeSpeechModal')
+    }
+  },
+  created () {
+  },
+  mounted () {
+  }
+}
+</script>
+<style scoped>
+
+</style>
