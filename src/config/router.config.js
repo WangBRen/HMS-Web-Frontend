d156e9e72d34a8e1f6a67fbb888be0d7c09df8a0
cao <2719241456@qq.com>
2023-01-13 15:44:44 +0800

    feat: 删除个人中心，修改个人设置页

--- src/config/router.config.js
+++ src/config/router.config.js
@@ -66,12 +66,6 @@ export const asyncRouterMap = [
         meta: { title: 'menu.account', icon: 'user', keepAlive: true, permission: ['user'] },
         children: [
           {
-            path: '/account/center',
-            name: 'center',
-            component: () => import('@/views/account/center'),
-            meta: { title: 'menu.account.center', keepAlive: true, permission: ['user'] }
-          },
-          {
             path: '/account/settings',
             name: 'settings',
             component: () => import('@/views/account/settings/Index'),
@@ -91,29 +85,6 @@ export const asyncRouterMap = [
                 component: () => import('@/views/account/settings/Security'),
                 meta: {
                   title: 'account.settings.menuMap.security',
-                  hidden: true,
-                  keepAlive: true,
-                  permission: ['user']
-                }
-              },
-              {
-                path: '/account/settings/custom',
-                name: 'CustomSettings',
-                component: () => import('@/views/account/settings/Custom'),
-                meta: { title: 'account.settings.menuMap.custom', hidden: true, keepAlive: true, permission: ['user'] }
-              },
-              {
-                path: '/account/settings/binding',
-                name: 'BindingSettings',
-                component: () => import('@/views/account/settings/Binding'),
-                meta: { title: 'account.settings.menuMap.binding', hidden: true, keepAlive: true, permission: ['user'] }
-              },
-              {
-                path: '/account/settings/notification',
-                name: 'NotificationSettings',
-                component: () => import('@/views/account/settings/Notification'),
-                meta: {
-                  title: 'account.settings.menuMap.notification',
                   hidden: true,
                   keepAlive: true,
                   permission: ['user']
