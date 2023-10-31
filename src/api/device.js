c91e44252340fdfb8c41d9293098709eea2bbdbc
cao <2719241456@qq.com>
2023-10-31 11:41:51 +0800

    feat: 设备管理

--- src/api/device.js
+++ src/api/device.js
@@ -2,7 +2,9 @@ import request from '@/utils/request'
 
 const api = {
   deviceReport: 'device-reports/-1/all',
-  reportDetail: (reportId) => `device-reports/-1/${reportId}`
+  reportDetail: (reportId) => `device-reports/-1/${reportId}`,
+  getProducts: 'products',
+  getDevice: (productId) => `products/${productId}/devices`
 }
 
 export function getDeviceReport (pages, mode, type) {
@@ -22,5 +24,27 @@ export function reportDetail (reportId) {
   return request({
     url: api.reportDetail(reportId),
     method: 'get'
+  })
+}
+
+export function getProducts (pages) {
+  return request({
+    url: api.getProducts,
+    method: 'get',
+    params: {
+      page: pages.page || 1,
+      size: pages.size || 10
+    }
+  })
+}
+
+export function getDevice (pages, productId) {
+  return request({
+    url: api.getDevice(productId),
+    method: 'get',
+    params: {
+      page: pages.page || 1,
+      size: pages.size || 10
+    }
   })
 }
