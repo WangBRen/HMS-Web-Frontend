b77ebe14ae9412f8cc377cfe685dce8fd2a04d78
cao <2719241456@qq.com>
2023-12-22 10:56:59 +0800

    feat: 统计图

--- src/views/detection/EcgDetail.vue
+++ src/views/detection/EcgDetail.vue
@@ -6,6 +6,7 @@
     @cancel="handleCancel"
     :footer="false"
   >
+    <!-- 心电 -->
     <div v-if="reportPro.reportType==='ecg'">
       <a-collapse>
         <a-collapse-panel key="1" :header="'原始数据：'+reportPro.data?.ecgList.length+'个'">
@@ -38,10 +39,11 @@
         </a-descriptions-item>
       </a-descriptions>
     </div>
+    <!-- 尿检 -->
     <div v-else style="margin: 0 10px;">
       <a-row :gutter="100">
         <a-col :span="12">
-          <a-table :columns="rgbColumns" :data-source="reportPro.data.originData" size="small" >
+          <a-table :columns="rgbColumns" :data-source="reportPro.data?.originData" size="small" :rowKey="(record, index) => index">
             <div slot="color" slot-scope="text, record">
               <div class="color-block" :style="{ backgroundColor: `rgb(${record.r},${record.g},${record.b})` }">
               </div>
@@ -49,9 +51,14 @@
           </a-table>
         </a-col>
         <a-col :span="12">
-          <a-table :columns="columns" :data-source="urineArr" :pagination="false" size="small">
-            //
-          </a-table>
+          <a-table :columns="columns" :data-source="urineArr" :pagination="false" size="small" :rowKey="(record, index) => index"></a-table>
+        </a-col>
+      </a-row>
+      <a-row :gutter="100">
+        <a-col :span="24">
+          <!-- 尿检rgb趋势图 -->
+          <!-- <v-chart :options="chartOptions" style="height: 400px;"></v-chart> -->
+          <div id="urineEcharts" style="width: 100%; height: 300px;"></div>
         </a-col>
       </a-row>
     </div>
@@ -60,6 +67,7 @@
 
 <script>
 import { reportDetail } from '@/api/device'
+import * as echarts from 'echarts'
 const errorArr = ['正常窦性心律', '停搏', '室颤或室速', 'R on T', '连续室性早搏', '二连发室早', '单个室早', '室早二联律', '室早三联律', '室速', '室缓', '起搏器未俘获', '起搏器未起搏', '漏搏', '正在学习', '', '过载信号', '信号幅度过小（信号弱）']
 const indexes = ['白细胞', '尿胆原', '微量白蛋白', '蛋白质', '胆红素', '葡萄糖', '抗坏血酸', '比重', '酮体', '亚硝酸盐', '肌酐', 'PH值', '隐血', '尿钙']
 const rgbColumns = [
@@ -117,6 +125,10 @@ export default {
       default: null
     }
   },
+  components: {
+    // 'v-chart': ECharts
+    // Line
+  },
   data () {
     return {
       urineArr: [],
@@ -125,31 +137,100 @@ export default {
       errorArr,
       title: '报告详情',
       reportPro: {},
-      ModalText: 'Content of the modal'
+      chartOptions: {
+        xAxis: {
+          type: 'category',
+          data: [] // 用于存储横坐标值（x 值）
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [
+          {
+            data: [], // 用于存储纵坐标值（r 值）
+            type: 'line',
+            smooth: true
+          }
+        ],
+        graphic: [] // 用于存储纵向虚线的数据
+      },
+      verticalLines: [] // 纵向虚线的数据
     }
   },
   mounted () {
     this.getDevice()
   },
   methods: {
+    drawChart (dataArr, results) {
+      // const verticalLinesArr = [23, 50, 80, 100, 140, 300]
+      // 截取后42位
+      const subArray = results?.slice(-42)
+
+      // 提取每三个数字中的第一个数字组成新数组
+      const verticalLinesArr = []
+      for (let i = 0; i < subArray.length; i += 3) {
+        verticalLinesArr.push(subArray[i])
+      }
+      // 提取 x 值和 r 值
+      const xData = dataArr.map(obj => obj.x)
+      const rData = dataArr.map(obj => obj.r)
+
+      var chartDom = document.getElementById('urineEcharts')
+      var myChart = echarts.init(chartDom)
+      var option
+
+      option = {
+        xAxis: {
+          type: 'category',
+          data: xData
+        },
+        yAxis: {
+          type: 'value'
+        },
+        tooltip: { // 添加 tooltip 配置项
+          trigger: 'axis'
+        },
+        series: [
+          {
+            data: rData,
+            type: 'line',
+            smooth: true,
+            markLine: {
+              symbol: 'none',
+              lineStyle: {
+                type: 'dashed',
+                color: 'red'
+              },
+              data: verticalLinesArr.map(value => ({
+                xAxis: value
+              }))
+            }
+          }
+        ]
+      }
+
+      option && myChart.setOption(option)
+    },
     async getDevice () {
       const res = await reportDetail(this.reportId)
       this.reportPro = res.data
-      const data = this.reportPro.data.fullResults
-      const urineArr = []
-      data.classes.map((item, index) => {
-        urineArr.push({
-          class: data.classes[index],
-          level: data.levels[index],
-          result: data.results[index],
-          find: data.findings[index]
-        })
-      })
-      this.urineArr = urineArr
       if (res.data.reportType === 'ecg') {
         this.title = '心电报告详情'
       } else {
+        const results = res.data.data.fullResults?.rgbResults
+        this.drawChart(res.data.data.originData, results)
         this.title = '尿检报告详情'
+        const data = this.reportPro.data.fullResults
+        const urineArr = []
+        data.classes.map((item, index) => {
+          urineArr.push({
+            class: data.classes[index],
+            level: data.levels[index],
+            result: data.results[index],
+            find: data.findings[index]
+          })
+        })
+        this.urineArr = urineArr
       }
       console.log('11', res.data)
     },
